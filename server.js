fb0cae5779b19787a12c219b200ef9d08cf5bd10
krislg420 <57952065+krislg420@users.noreply.github.com>
2020-03-02 19:17:32 -0600

    added events

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js.js")(app);
-require("./routes/api-routes.js.js")(app);
-require("./routes/video-routes.js.js")(app);
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+require("./routes/video-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function () {
